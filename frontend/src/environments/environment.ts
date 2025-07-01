3d46d0e1734e85b54b0e8b60300ff29e9c7aa02e
Pedro Henrique <pedrohen.dev@gmail.com>
2025-07-01 16:28:17 +0000

    ajuste de configuração de ambiente e rotas

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
+//environment para o ambiente de desenvolvimento
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080/api'
-  //apiUrl: 'https://4200-pedrohgo-proj1angular-7xyo5c7jdnv.ws-us120.gitpod.io/'
   apiUrl: 'https://8080-pedrohgo-proj1angular-7xyo5c7jdnv.ws-us120.gitpod.io/api'
 };
