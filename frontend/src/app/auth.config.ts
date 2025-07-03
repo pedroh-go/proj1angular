1f81ef90cdef598518372011b671957fd452071e
Pedro Henrique <pedrohen.dev@gmail.com>
2025-07-03 22:36:51 +0000

    feature: criando login federado, ajustes para ambiente gitpod

--- frontend/src/app/auth.config.ts
+++ frontend/src/app/auth.config.ts
@@ -1,7 +1,8 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  issuer: 'http://localhost:8081/realms/proj1angular',
+  //issuer: 'http://localhost:8081/realms/proj1angular', //local
+  issuer: 'https://8081-pedrohgo-proj1angular-7xyo5c7jdnv.ws-us120.gitpod.io/realms/proj1angular',
   redirectUri: window.location.origin,
   clientId: 'proj1angular-app',
   responseType: 'code',
