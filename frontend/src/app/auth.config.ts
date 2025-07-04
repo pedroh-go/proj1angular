e287e1ebef8378611cc06c2941a27bf8a16de9b8
Pedro Henrique <pedrohen.dev@gmail.com>
2025-07-04 00:27:48 -0300

    login federado ajustado para funcionar em dev e produ sem alterar as rotas via código

--- frontend/src/app/auth.config.ts
+++ frontend/src/app/auth.config.ts
@@ -1,10 +1,13 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
+import { environment } from '../environments/environment';
 
+/**
+ * variáveis abaixo definidas nos environment.ts(dev) ou environment.prod.ts(produção)
+ */
 export const authConfig: AuthConfig = {
-  //issuer: 'http://localhost:8081/realms/proj1angular', //local
-  issuer: 'https://8081-pedrohgo-proj1angular-7xyo5c7jdnv.ws-us120.gitpod.io/realms/proj1angular',
-  redirectUri: window.location.origin,
-  clientId: 'proj1angular-app',
+  issuer: environment.issuer,
+  redirectUri: environment.redirectUri,
+  clientId: environment.clientId,
   responseType: 'code',
   scope: 'openid profile email',
   showDebugInformation: true,
