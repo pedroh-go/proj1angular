a72d89a0c70b5d48aec09fbc792b83c5abb41329
Pedro Henrique <pedrohen.dev@gmail.com>
2025-07-17 01:19:33 -0300

    personalização de frontend

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon'; // Importe o MatIconModule
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +13,13 @@ import { MatIconModule } from '@angular/material/icon'; // Importe o MatIconModu
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  loginWithKeycloak() {
-    window.location.href = 'http://localhost:8081/realms/proj1angular/protocol/openid-connect/auth?client_id=proj1angular-app&redirect_uri=http://localhost:4200&response_type=code&scope=openid';
+  constructor(private authService: AuthService) {}
+
+  loginWithKeycloak(): void {
+    this.authService.loginWithKeycloak();
   }
 
-  loginWithGoogle() {
-    window.location.href = 'http://localhost:8081/realms/proj1angular/protocol/openid-connect/auth?client_id=proj1angular-app&redirect_uri=http://localhost:4200&response_type=code&scope=openid&kc_idp_hint=google';
+  loginWithGoogle(): void {
+    this.authService.loginWithGoogle();
   }
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { authConfig } from '../auth.config'; 
+import { authConfig } from '../auth.config';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+
+  private issuer = environment.issuer;
+  private clientId = environment.clientId;
+  private redirectUri = environment.redirectUri;
+
   constructor(private oauthService: OAuthService) {
     this.configure();
   }
 
-  private configure() {
+  private configure(): void {
     this.oauthService.configure(authConfig);
     this.oauthService.loadDiscoveryDocumentAndTryLogin();
   }
 
   login(): void {
     this.oauthService.initCodeFlow();
+  }
+
+  loginWithKeycloak(): void {
+    const url = `${this.issuer}/protocol/openid-connect/auth?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&response_type=code&scope=openid`;
+    window.location.href = url;
+  }
+
+  loginWithGoogle(): void {
+    const url = `${this.issuer}/protocol/openid-connect/auth?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&response_type=code&scope=openid&kc_idp_hint=google`;
+    window.location.href = url;
   }
 
   logout(): void {
