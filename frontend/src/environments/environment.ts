ac3159474e6d034e7ee8a4b2c3ff11206dff4756
pedro.goncalves <pedro.goncalves@unb.br>
2025-07-28 13:42:15 -0300

    centralização de portas no .env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,10 +3,10 @@ console.log('🔧 Environment carregado: DESENVOLVIMENTO');
 export const environment = {
   production: false,
 
-  // Backend API
+  // Backend API: a porta abaixo deve ser igual a variável BACKEND_PORT definida no .env
   apiUrl: 'http://localhost:8080/api',
 
-  // Autenticação com Keycloak (ambiente local)
+  // Autenticação com Keycloak (ambiente local): a porta abaixo deve ser igual a variável KEYCLOAK_PORT definida no .env
   issuer: 'http://localhost:8081/realms/proj1angular',
   clientId: 'proj1angular-app',
   redirectUri: window.location.origin, //detecta automaticamente a origem atual da aplicação local ou não
